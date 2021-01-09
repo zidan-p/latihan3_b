4e255045e78cd32fdc074515db30a8e1d9b140f8
Zidan Putra Rahman <68506833+zidan-p@users.noreply.github.com>
2021-01-09 09:09:16 +0700

    Add files via upload

---
+++ latihan3_b.java
@@ -0,0 +1,22 @@
+
+package latihanPBO;
+
+
+public class latihan3_b {
+
+
+    public static void main(String[] args) {
+        int p = 5;  //mendeklarasikan variabel beserta memberi nilai
+        int i=10;   // mendeklarasikan variabel beserta memberi nilai
+        int t=5;    //mendeklarasikan variabel beserta memberi nilai
+        
+        System.out.println("nilai panjang balok : "+p); //menampilkan kalimat berserta varibel
+        System.out.println("nilai lebar balok : "+i);   //menampilkan kalimat berserta varibel
+        System.out.println("nilai tinggin balok : "+t); //menampilkan kalimat berserta varibel
+        System.out.println("===============================");
+        System.out.println("menghitung luas permukaan balok");
+        System.out.println("================================");
+        System.out.println("luas permukaan balok = "+((2*p*i)+(2*p*t)+(2*i*t)));    //menampilkan kalimta berserta hasil dari operasi hitung
+    }
+    
+}
